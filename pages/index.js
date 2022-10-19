cac7a0f04e92321b1d33a11946f0ab3bca29108d
arash <arashesna.dev@gmail.com>
2022-10-19 11:31:16 +0330

    some

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,77 @@
 export default function Home() {
   return (
     <div className="container">
-      <h1 className="name3">Hello im Arash Esnaashari</h1>
+      <h1 className="name3">Im Arash Esnaashari</h1>
       <div className="vscode">
-        <p><span style={{color:"#569cd6"}}>function</span> <span style={{color:"green"}}>Man</span>() {'{'} </p>
-          <p className="bist"><span style={{color:"#569cd6"}}>this</span>.<span style={{color:"#9CDCFE"}}>name</span> = <span style={{color:"#ce9178"}}>"arash"</span>;</p>
-          <p className="bist"><span style={{color:"#569cd6"}}>this</span>.<span style={{color:"#9CDCFE"}}>age</span> = <span style={{color:"#b5cea8"}}>18</span>; </p>
-          <p className="bist"><span style={{color:"#569cd6"}}>this</span>.<span style={{color:"#9CDCFE"}}>roles</span> = [<span style={{color:"#ce9178"}}>'son'</span>,<span style={{color:"#ce9178"}}>'brother'</span>,<span style={{color:"#ce9178"}}>'student'</span>]</p>
-          <p className="bist"><span style={{color:"#569cd6"}}>this</span>.<span style={{color:"#9CDCFE"}}>hobbies</span> = [<span style={{color:"#ce9178"}}>'listening'</span>,<span style={{color:"#ce9178"}}>'watching'</span>,<span style={{color:"#ce9178"}}>'learning'</span>]</p>
-          <p className="bist"><span style={{color:"#569cd6"}}>this</span>.<span style={{color:"#DCDCAA"}}>as_a_Devops</span> = <span style={{color:"#569cd6"}}>function</span> () {'{'}</p>
-            <p className="chel"><span style={{color:"#9CDCFE"}}>console</span>.<span style={{color:"#DCDCAA"}}>log</span>(<span style={{color:"#ce9178"}}>"deploy and grow webapp"</span>) </p>
-          <p className="bist">{'};'}</p>
-          <p className="bist"><span style={{color:"#569cd6"}}>this</span>.<span style={{color:"#DCDCAA"}}>as_a_BackendDev</span> = <span style={{color:"#569cd6"}}>function</span> () {'{'}</p>
-            <p className="chel"><span style={{color:"#9CDCFE"}}>console</span>.<span style={{color:"#DCDCAA"}}>log</span>(<span style={{color:"#ce9178"}}>"develop fast and scalable webapp"</span>)</p>
-          <p className="bist">{';'}</p>
-          <p className="bist"><span style={{color:"#569cd6"}}>this</span>.<span style={{color:"#DCDCAA"}}>as_a_FrontendDev</span> = <span style={{color:"#569cd6"}}>function</span> () {'{'}</p>
-            <p className="chel"><span style={{color:"#9CDCFE"}}>console</span>.<span style={{color:"#DCDCAA"}}>log</span>(<span style={{color:"#ce9178"}}>"design cool & multy platform webapp "</span>)</p>
-          <p className="bist">{'};'}</p>
-        <p>{'};'}</p>
+        <p>
+          <span style={{ color: "#569cd6" }}>function</span>{" "}
+          <span style={{ color: "green" }}>Man</span>() {"{"}{" "}
+        </p>
+        <p className="bist">
+          <span style={{ color: "#569cd6" }}>this</span>.
+          <span style={{ color: "#9CDCFE" }}>name</span> ={" "}
+          <span style={{ color: "#ce9178" }}>"arash"</span>;
+        </p>
+        <p className="bist">
+          <span style={{ color: "#569cd6" }}>this</span>.
+          <span style={{ color: "#9CDCFE" }}>age</span> ={" "}
+          <span style={{ color: "#b5cea8" }}>18</span>;{" "}
+        </p>
+        <p className="bist">
+          <span style={{ color: "#569cd6" }}>this</span>.
+          <span style={{ color: "#9CDCFE" }}>roles</span> = [
+          <span style={{ color: "#ce9178" }}>'son'</span>,
+          <span style={{ color: "#ce9178" }}>'brother'</span>,
+          <span style={{ color: "#ce9178" }}>'student'</span>]
+        </p>
+        <p className="bist">
+          <span style={{ color: "#569cd6" }}>this</span>.
+          <span style={{ color: "#9CDCFE" }}>hobbies</span> = [
+          <span style={{ color: "#ce9178" }}>'listening'</span>,
+          <span style={{ color: "#ce9178" }}>'watching'</span>,
+          <span style={{ color: "#ce9178" }}>'learning'</span>]
+        </p>
+        <p className="bist">
+          <span style={{ color: "#569cd6" }}>this</span>.
+          <span style={{ color: "#DCDCAA" }}>as_a_Devops</span> ={" "}
+          <span style={{ color: "#569cd6" }}>function</span> () {"{"}
+        </p>
+        <p className="chel">
+          <span style={{ color: "#9CDCFE" }}>console</span>.
+          <span style={{ color: "#DCDCAA" }}>log</span>(
+          <span style={{ color: "#ce9178" }}>"deploy and grow webapp"</span>){" "}
+        </p>
+        <p className="bist">{"};"}</p>
+        <p className="bist">
+          <span style={{ color: "#569cd6" }}>this</span>.
+          <span style={{ color: "#DCDCAA" }}>as_a_BackendDev</span> ={" "}
+          <span style={{ color: "#569cd6" }}>function</span> () {"{"}
+        </p>
+        <p className="chel">
+          <span style={{ color: "#9CDCFE" }}>console</span>.
+          <span style={{ color: "#DCDCAA" }}>log</span>(
+          <span style={{ color: "#ce9178" }}>
+            "develop fast and scalable webapp"
+          </span>
+          )
+        </p>
+        <p className="bist">{";"}</p>
+        <p className="bist">
+          <span style={{ color: "#569cd6" }}>this</span>.
+          <span style={{ color: "#DCDCAA" }}>as_a_FrontendDev</span> ={" "}
+          <span style={{ color: "#569cd6" }}>function</span> () {"{"}
+        </p>
+        <p className="chel">
+          <span style={{ color: "#9CDCFE" }}>console</span>.
+          <span style={{ color: "#DCDCAA" }}>log</span>(
+          <span style={{ color: "#ce9178" }}>
+            "design cool & multy platform webapp "
+          </span>
+          )
+        </p>
+        <p className="bist">{"};"}</p>
+        <p>{"};"}</p>
       </div>
-   
     </div>
   );
 }
